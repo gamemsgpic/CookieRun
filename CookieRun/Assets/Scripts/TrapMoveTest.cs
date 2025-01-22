f3d020e40f72af070fad06111d295f9138f80d28
문세진 <lunatic1209@naver.com>
2025-01-23 06:35:06 +0900

    무한맵 구현용 테스트 맵 제작

--- CookieRun/Assets/Scripts/TrapMoveTest.cs
+++ CookieRun/Assets/Scripts/TrapMoveTest.cs
@@ -8,11 +8,11 @@ public class TrapMoveTest : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.Translate(Vector3.left * currentSpeed * Time.deltaTime, Space.World);
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime, Space.World);
 
         if (transform.position.x < -15f)
         {
-            transform.position = new Vector3(9.6f, -2.5f, 0f);
+            transform.position = new Vector2(9.6f, -2.5f);
         }
     }
 }
