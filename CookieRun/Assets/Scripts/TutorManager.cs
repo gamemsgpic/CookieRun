6a108368cf3c3132866acd3744cc5e65c1287aa8
문세진 <lunatic1209@naver.com>
2025-02-12 11:54:54 +0900

    튜토리얼 윈도우 구현

---
+++ CookieRun/Assets/Scripts/TutorManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorManager : MonoBehaviour
+{
+    public Sprite[] TutorSprites;
+    private Image image;
+    private int currentIndex = 0;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        currentIndex = 0;
+        image.sprite = TutorSprites[currentIndex];
+    }
+
+    public void NextPage()
+    {
+        currentIndex++;
+        if (currentIndex >= TutorSprites.Length)
+        {
+            currentIndex = 0;
+        }
+
+        image.sprite = TutorSprites[currentIndex];
+    }
+
+    public void PreviousPage()
+    {
+        currentIndex--;
+        if (currentIndex < 0)
+        {
+            currentIndex = TutorSprites.Length - 1;
+        }
+
+        image.sprite = TutorSprites[currentIndex];
+    }
+
+
+}
