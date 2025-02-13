d4ac21bbec5a009eba46cfa8c5427a384d536438
문세진 <lunatic1209@naver.com>
2025-02-13 23:49:22 +0900

    튜토리얼 수정

--- CookieRun/Assets/Scripts/TutorManager.cs
+++ CookieRun/Assets/Scripts/TutorManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TutorManager : MonoBehaviour
 {
     public Sprite[] TutorSprites;
+    public TextMeshProUGUI nextButtonText;
+    public TextMeshProUGUI priviousButtonText;
     private Image image;
     private int currentIndex = 0;
 
@@ -16,10 +19,25 @@ public class TutorManager : MonoBehaviour
         image.sprite = TutorSprites[currentIndex];
     }
 
+    private void OnEnable()
+    {
+        currentIndex = 0;
+        priviousButtonText.color = Color.clear;
+    }
+
     public void NextPage()
     {
         currentIndex++;
-        if (currentIndex >= TutorSprites.Length)
+        priviousButtonText.color = Color.white;
+        if (currentIndex == TutorSprites.Length - 1)
+        {
+            nextButtonText.color = Color.clear;
+        }
+        else if (currentIndex < TutorSprites.Length - 1)
+        {
+            nextButtonText.color = Color.white;
+        }
+        else if (currentIndex >= TutorSprites.Length)
         {
             currentIndex = TutorSprites.Length - 1;
         }
@@ -30,7 +48,16 @@ public class TutorManager : MonoBehaviour
     public void PreviousPage()
     {
         currentIndex--;
-        if (currentIndex < 0)
+        nextButtonText.color = Color.white;
+        if (currentIndex == 0)
+        {
+            priviousButtonText.color = Color.clear;
+        }
+        else if (currentIndex > 0)
+        {
+            priviousButtonText.color = Color.white;
+        }
+        else if (currentIndex < 0)
         {
             currentIndex = 0;
         }
